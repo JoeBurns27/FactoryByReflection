532ccc5cdd5e9d4126ccefb3f882f0ff2b6b4007
JoeBurns27 <jebur27@outlook.com>
2014-10-23 04:55:54 -0500

    Added new project (and test project) with a Security Factory in a different assembly than the Security Managers

---
+++ src/Security/Factory/Factory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jebur27.Security;
+using Jebur27.Security.Interfaces;
+using Jebur27.FactoryBase;
+
+namespace Jebur27.Security.Factory
+{
+    public class Factory : BaseFactory 
+    {
+        public ISecurityManager GetSecurityManager(string securityManageName)
+        {
+            ISecurityManager secMan;
+            secMan = (ISecurityManager)CreateObject(securityManageName, GetType());
+            if (secMan == null)
+                throw new Jebur27.FactoryBase.Exceptions.TypeNotFoundFactoryException(securityManageName);
+            return secMan;
+        }
+
+    }
+}
---
+++ src/Security/Factory/Test/SecurityFactoryTests2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jebur27.Security;
+using Jebur27.FactoryBase.Exceptions;
+using Jebur27.Security.Factory;
+using Jebur27.Security.Interfaces;
+using Jebur27.Security.Factory;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetSecurityManagerOne()
+        {
+            Factory target = new Factory();
+            ISecurityManager securityManager = target.GetSecurityManager("Jebur27.Security.SecurityManagerOne");
+            SecurityManagerOne securityManagerOne = new SecurityManagerOne();
+            Assert.AreEqual(securityManager.GetType(), securityManagerOne.GetType());
+        }
+
+    }
+}
