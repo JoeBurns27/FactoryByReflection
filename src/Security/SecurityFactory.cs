306c44e67929e913cfd6936e7278f93a34d953e7
JoeBurns27 <jebur27@outlook.com>
2014-10-25 08:19:42 -0500

    Renamed parameters.

--- src/Security/SecurityFactory.cs
+++ src/Security/SecurityFactory.cs
@@ -16,49 +16,49 @@ namespace Jebur27.Security.Factory
         /// <summary>
         /// Returns the named Security Manager if fould, else throws
         /// </summary>
-        /// <param name="securityManageName">Fully qualified name of the Security Manager</param>
+        /// <param name="securityManagerName">Fully qualified name of the Security Manager</param>
         /// <exception cref="Jebur27.FactoryBase.Exceptions.TypeNotFoundFactoryException">Thrown when named Type not found</exception>
         /// <returns>object of named type (ISecurityManager)</returns>
-        public ISecurityManager GetSecurityManager(string securityManageName)
+        public ISecurityManager GetSecurityManager(string securityManagerName)
         {
-            ISecurityManager securityManager = (ISecurityManager)CreateObject(securityManageName, GetType());
+            ISecurityManager securityManager = (ISecurityManager)CreateObject(securityManagerName, GetType());
             if (securityManager == null)
-                throw new Jebur27.FactoryBase.Exceptions.TypeNotFoundFactoryException(securityManageName);
+                throw new Jebur27.FactoryBase.Exceptions.TypeNotFoundFactoryException(securityManagerName);
             return securityManager;
         }
 
         /// <summary>
         /// Returns the named Security Manager if fould, else throws
         /// </summary>
-        /// <param name="securityManageName">Fully qualified name of the Security Manager</param>
+        /// <param name="securityManagerName">Fully qualified name of the Security Manager</param>
         /// <param name="userName">Constructor parameter</param>
         /// <exception cref="Jebur27.FactoryBase.Exceptions.TypeNotFoundFactoryException">Thrown when named Type not found</exception>
         /// <returns>object of named type (ISecurityManager)</returns>
-        public ISecurityManager GetSecurityManager(string securityManageName, string userName)
+        public ISecurityManager GetSecurityManager(string securityManagerName, string userName)
         {
             object[] parms = new object[] { userName };
 
-            ISecurityManager securityManager = (ISecurityManager)CreateObject(securityManageName, GetType(), parms);
+            ISecurityManager securityManager = (ISecurityManager)CreateObject(securityManagerName, GetType(), parms);
             if (securityManager == null)
-                throw new Jebur27.FactoryBase.Exceptions.TypeNotFoundFactoryException(securityManageName);
+                throw new Jebur27.FactoryBase.Exceptions.TypeNotFoundFactoryException(securityManagerName);
             return securityManager;
         }
 
         /// <summary>
         /// Returns the named Security Manager if fould, else throws
         /// </summary>
-        /// <param name="securityManageName">Fully qualified name of the Security Manager</param>
+        /// <param name="securityManagerName">Fully qualified name of the Security Manager</param>
         /// <param name="userName">Constructor parameter</param>
         /// <param name="userSecurityLevel">Constructor parameter</param>
         /// <exception cref="Jebur27.FactoryBase.Exceptions.TypeNotFoundFactoryException">Thrown when named Type not found</exception>
         /// <returns>object of named type (ISecurityManager)</returns>
-        public ISecurityManager GetSecurityManager(string securityManageName, string userName, int userSecurityLevel)
+        public ISecurityManager GetSecurityManager(string securityManagerName, string userName, int userSecurityLevel)
         {
             object[] parms = new object[] { userName, userSecurityLevel };
 
-            ISecurityManager securityManager = (ISecurityManager)CreateObject(securityManageName, GetType(), parms);
+            ISecurityManager securityManager = (ISecurityManager)CreateObject(securityManagerName, GetType(), parms);
             if (securityManager == null)
-                throw new Jebur27.FactoryBase.Exceptions.TypeNotFoundFactoryException(securityManageName);
+                throw new Jebur27.FactoryBase.Exceptions.TypeNotFoundFactoryException(securityManagerName);
             return securityManager;
         }
 
